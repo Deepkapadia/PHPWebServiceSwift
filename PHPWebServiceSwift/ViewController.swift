5ff0a952c64c363a90d7b171670e06a01c0824af
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-18 12:20:38 +0530

    PHP_Web_Service_Swift
    PHP Web Service Swift Sample

--- PHPWebServiceSwift/ViewController.swift
+++ PHPWebServiceSwift/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,URLSessionDelegate {
 
+    @IBOutlet weak var txtid: UITextField!
+    
+    @IBOutlet weak var txtfn: UITextField!
+    
+    @IBOutlet weak var txtag: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,84 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnselect(_ sender: Any) {
+        
+        let str = "http://localhost/Ankit_Webservice/select.php";
+        
+        let url =  URL(string: str);
+        
+        let requet = URLRequest(url: url!);
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil);
+        
+        let task = session.dataTask(with: requet) { (data, responce, err) in
+            
+            let result = String(data: data!, encoding: String.Encoding.utf8);
+            
+            print(result!);
+        }
+        
+        task.resume();
+    }
+    
+    @IBAction func btninsert(_ sender: Any) {
+        
+        let str = "http://localhost/Ankit_Webservice/insert.php?fname=\(txtfn.text!)&age=\(txtag.text!)";
+        
+        let url = URL(string: str);
+        
+        let requet = URLRequest(url: url!);
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil);
+        
+        let task = session.dataTask(with: requet) { (data, responce, err) in
+            
+            let result = String(data: data!, encoding: String.Encoding.utf8);
+            
+            print(result!);
+        }
+        
+        task.resume();
+    }
+    
+    @IBAction func btnupdate(_ sender: Any) {
+        
+        let str = "http://localhost/Ankit_Webservice/update.php?sid=\(txtid.text)&fname=\(txtfn.text!)&age=\(txtag.text!)";
+        
+        let url = URL(string: str);
+        
+        let requet = URLRequest(url: url!);
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil);
+        
+        let task = session.dataTask(with: requet) { (data, responce, err) in
+            
+            let result = String(data: data!, encoding: String.Encoding.utf8);
+            
+            print(result!);
+        }
+        
+        task.resume();
+    }
 
+    @IBAction func btndelete(_ sender: Any) {
+        
+        let str = "http://localhost/Ankit_Webservice/delete.php?sid=\(txtid.text)&fname=\(txtfn.text!)&age=\(txtag.text!)";
+        
+        let url = URL(string: str);
+        
+        let requet = URLRequest(url: url!);
+        
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil);
+        
+        let task = session.dataTask(with: requet) { (data, responce, err) in
+            
+            let result = String(data: data!, encoding: String.Encoding.utf8);
+            
+            print(result!);
+        }
+        
+        task.resume();
+    }
 }
 
